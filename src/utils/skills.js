c367047ad601ded9aafcdad9e535f48687e63fb6
kikiabdilah <kiensa1234@gmail.com>
2025-11-12 14:46:36 +0700

    fixing bug

--- src/utils/skills.js
+++ src/utils/skills.js
@@ -1,49 +1,16 @@
-import { VscCode } from "react-icons/vsc";
-import { BiLogoTailwindCss } from "react-icons/bi";
-import { BiLogoJavascript, BiLogoReact, BiLogoPhp, BiLogoGit } from "react-icons/bi";
+import { VscVscode } from "react-icons/vsc";
+import { BiLogoTailwindCss, BiLogoJavascript, BiLogoReact, BiLogoPhp, BiLogoGit } from "react-icons/bi";
 import { SiLaravel, SiMysql } from "react-icons/si";
 
 const skills = [
-  {
-    name: "VS Code",
-    type: "react",
-    icon: VscCode,
-  },
-  {
-    name: "Tailwind CSS",
-    type: "react",
-    icon: BiLogoTailwindCss,
-  },
-  {
-    name: "JavaScript",
-    type: "react",
-    icon: BiLogoJavascript,
-  },
-  {
-    name: "React",
-    type: "react",
-    icon: BiLogoReact,
-  },
-  {
-    name: "PHP",
-    type: "react",
-    icon: BiLogoPhp,
-  },
-  {
-    name: "Laravel",
-    type: "react",
-    icon: SiLaravel,
-  },
-  {
-    name: "MySQL",
-    type: "react",
-    icon: SiMysql,
-  },
-  {
-    name: "Git",
-    type: "react",
-    icon: BiLogoGit,
-  },
+   { name: "VS Code", icon: VscVscode, color: "#007ACC" },
+  { name: "Tailwind CSS", icon: BiLogoTailwindCss, color: "#38BDF8" },
+  { name: "JavaScript", icon: BiLogoJavascript, color: "#F7DF1E" },
+  { name: "React", icon: BiLogoReact, color: "#61DAFB" },
+  { name: "PHP", icon: BiLogoPhp, color: "#777BB4" },
+  { name: "Laravel", icon: SiLaravel, color: "#FF2D20" },
+  { name: "MySQL", icon: SiMysql, color: "#00758F" },
+  { name: "Git", icon: BiLogoGit, color: "#F05032" },
 ];
 
 export default skills;
